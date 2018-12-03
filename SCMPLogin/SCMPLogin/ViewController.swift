7c1d43594134ed70ac15ef6a46c47cfe22b2731d
normanlys <limyueshing@gmail.com>
2018-12-03 19:48:52 +0800

    Create app

---
+++ SCMPLogin/SCMPLogin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SCMPLogin
+//
+//  Created by Norman Lim on 3/12/2018.
+//  Copyright © 2018 Norman Lim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
